ce6a3163c1d6d6fd1133c0920989291117f23b3f
Fazil Babu <fazil47babu@gmail.com>
2020-10-02 02:34:24 +0530

    Stremlit

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"fazil47babu@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
